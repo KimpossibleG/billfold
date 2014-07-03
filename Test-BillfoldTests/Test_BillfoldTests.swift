e986c6b3f7e4ba093ff99bfc659fe427ac1ccff4
Kim Girard <kimberley.girard@gmail.com>
2014-07-03 17:05:02 -0500

    reorg file structure for test

---
+++ Test-BillfoldTests/Test_BillfoldTests.swift
@@ -0,0 +1,35 @@
+//
+//  Test_BillfoldTests.swift
+//  Test-BillfoldTests
+//
+//  Created by Kim Girard on 2014-07-03.
+//  Copyright (c) 2014 Kim Girard. All rights reserved.
+//
+
+import XCTest
+
+class Test_BillfoldTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
