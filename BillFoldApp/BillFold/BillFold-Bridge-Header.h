8e6812485508a6470c5b0b55a3d75a15dece2478
Kim Girard <kimberley.girard@gmail.com>
2014-07-03 21:17:07 -0500

    integrate tesseract

---
+++ BillFoldApp/BillFold/BillFold-Bridge-Header.h
@@ -0,0 +1,10 @@
+//
+//  BillFold-Bridge-Header.h
+//  BillFold
+//
+//  Created by Kim Girard on 2014-07-03.
+//  Copyright (c) 2014 Michael Pourhadi. All rights reserved.
+//
+
+#import "tesseract.h"
+//#import "/tesseract-ios/tesseract.h"
---
+++ BillFoldApp/BillFold/BillFold/STesseract.swift
@@ -0,0 +1,14 @@
+//
+//  STesseract.swift
+//  BillFold
+//
+//  Created by Kim Girard on 2014-07-03.
+//  Copyright (c) 2014 Michael Pourhadi. All rights reserved.
+//
+
+import Foundation
+
+
+class STesseract: Tesseract {
+    
+}
