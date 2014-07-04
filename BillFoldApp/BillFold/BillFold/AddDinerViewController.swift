0922d857782544d8934e037bb1c9887f2c7e3bd2
Mikee Pourhadi <mikeepourhadi@gmail.com>
2014-07-03 20:07:55 -0500

    add MIKEEEEEEEE

---
+++ BillFoldApp/BillFold/BillFold/AddDinerViewController.swift
@@ -0,0 +1,72 @@
+//
+//  AddViewController.swift
+//  TodoApp
+//
+//  Created by Michael Pourhadi on 7/3/14.
+//  Copyright (c) 2014 Michael Pourhadi. All rights reserved.
+//
+
+import UIKit
+
+class AddDinerViewController: UIViewController {
+    
+    @IBOutlet strong var addDinerField: UITextField = UITextField()
+    
+    init(coder aDecoder: NSCoder!) {
+        super.init(coder: aDecoder)
+    }
+    
+    init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        // Custom initialization
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func addDinerTap(sender: AnyObject) {
+        var userDefaults:NSUserDefaults = NSUserDefaults.standardUserDefaults()
+        
+        var dinerList:NSMutableArray? = userDefaults.objectForKey("dinerList") as? NSMutableArray
+        
+        var dataSet:NSMutableDictionary = NSMutableDictionary()
+        dataSet.setObject(addDinerField.text, forKey: "dinerName")
+        
+        if dinerList {
+            var newMutableList:NSMutableArray = NSMutableArray()
+            
+            for dict:AnyObject in dinerList!{
+                newMutableList.addObject(dict as NSDictionary)
+            }
+            
+            userDefaults.removeObjectForKey("dinerList")
+            newMutableList.addObject(dataSet)
+            userDefaults.setObject(newMutableList, forKey: "dinerList")
+            
+        } else {
+            dinerList = NSMutableArray()
+            dinerList!.addObject(dataSet)
+            userDefaults.setObject(dinerList, forKey: "dinerList")
+        }
+        userDefaults.synchronize()
+        self.navigationController.popToRootViewControllerAnimated(true)
+    }
+    /*
+    // #pragma mark - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue?, sender: AnyObject?) {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+    }
+    */
+    
+}
