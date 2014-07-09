b2daeac43799e4afd9a6ac2ae91a8bb051f9cc42
Mikee Pourhadi <mikeepourhadi@gmail.com>
2014-07-08 19:19:26 -0500

    add prevent segue method if photo is nil

--- BillFoldApp/BillFold/BillFold/PhotoViewController.swift
+++ BillFoldApp/BillFold/BillFold/PhotoViewController.swift
@@ -14,17 +14,44 @@ class PhotoViewController: UIViewController, UIImagePickerControllerDelegate, UI
     let activityIndicator = UIActivityIndicatorView(activityIndicatorStyle: UIActivityIndicatorViewStyle.WhiteLarge)
     var overLayLoader = UIView()
     
-    func showLoader() {
+    func makeSpinner() {
         overLayLoader.frame = UIScreen.mainScreen().bounds
         overLayLoader.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.5)
         activityIndicator.startAnimating()
         activityIndicator.center = overLayLoader.center
         overLayLoader.addSubview(activityIndicator)
         navigationController.view.addSubview(overLayLoader)
+        overLayLoader.hidden = true
+    }
+    
+    func loadImage() {
+        var image:UIImage = self.image!.image
+        var imageString:NSString = TesseractController.recognizeImage(image) as NSString
+        var foodCollection = TesseractController.regexDo(imageString)
+        sharedFoodController.foodAndPrices = foodCollection
+        sharedRegexController.deleteSubtotal(foodCollection)
+        sharedRegexController.summarizeTaxes(foodCollection)
+    }
+    
+    override func shouldPerformSegueWithIdentifier(identifier: String!, sender: AnyObject!) -> Bool {
+        if self.image!.image === nil {
+            return false
+        } else {
+            return true
+        }
+    }
+    
+    override func performSegueWithIdentifier(identifier: String!, sender: AnyObject!) {
+        
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        loadImage()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        makeSpinner()
         photoButton.title = "Photo"
         navigationController.navigationBar.barTintColor = lightColor
         navigationController.navigationBar
@@ -34,7 +61,6 @@ class PhotoViewController: UIViewController, UIImagePickerControllerDelegate, UI
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
     
     // Outlets
     @IBOutlet var photoButton : UIBarButtonItem
@@ -58,19 +84,6 @@ class PhotoViewController: UIViewController, UIImagePickerControllerDelegate, UI
             capture.sourceType = .PhotoLibrary
         }
         self.presentViewController(capture, animated: true, completion: nil)
-        
-    }
-    
-    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
-        showLoader()
-        var image:UIImage = UIImage(named: "photo.JPG")
-        var imageString:NSString = TesseractController.recognizeImage(image) as NSString
-        var foodCollection = TesseractController.regexDo(imageString)
-        //        println(foodCollection)
-        sharedFoodController.foodAndPrices = foodCollection
-        
-        sharedRegexController.deleteSubtotal(foodCollection)
-        sharedRegexController.summarizeTaxes(foodCollection)
     }
     
     // Delegate Methods
@@ -81,8 +94,5 @@ class PhotoViewController: UIViewController, UIImagePickerControllerDelegate, UI
         Instructions.hidden = true
         Welcome.hidden = true
     }
-    
-    
-    
     
 }
